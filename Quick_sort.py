e59ff8cbb3d7ec7648b39926caca0734878c8922
Nguyễn Văn Chung <56922083+chungnguyen10012000@users.noreply.github.com>
2021-03-13 16:11:18 +0700

    add quick sort middle pivot

--- Quick_sort.py
+++ Quick_sort.py
@@ -2,46 +2,71 @@ def Quick_sort1(lst,left,right):
     if left >= right :
         return
     else:
-        p = lst[left]
-        j = left + 1
-        k = right
-        while (j<=k):
-            while(lst[j] < p): j += 1
-            while(lst[k] > p): k -= 1
-            if j <= k:
-                lst[j],lst[k] = lst[k],lst[j]
-                j += 1
-                k -= 1
+        pivot = lst[left]
+        print(pivot)
+        i = left + 1
+        j = right
+        while (i<=j):
+            while(lst[i] < pivot): i += 1
+            while(lst[j] > pivot): j -= 1
+            if i <= j:
+                lst[i],lst[j] = lst[j],lst[i]
+                i += 1
+                j -= 1
             else:
                 break
-        lst[left] , lst[k]  = lst[k] , lst[left]
-        Quick_sort1(lst,left,k-1)
-        Quick_sort1(lst,k+1,right)
+            print(lst)
+        lst[left] , lst[j]  = lst[j] , lst[left]
+        Quick_sort1(lst,left,j-1)
+        Quick_sort1(lst,j+1,right)
 
 def Quick_sort2(lst,left,right):
     if left >= right :
         return
     else:
-        p = lst[right]
-        j = left
-        k = right - 1
-        while (j<=k):
-            while(lst[j] < p): j += 1
-            while(lst[k] > p): k -= 1
-            if j <= k:
-                lst[j],lst[k] = lst[k],lst[j]
-                j += 1
-                k -= 1
+        pivot = lst[right]
+        print(pivot)
+        i = left
+        j = right - 1
+        while (i<=j):
+            while(lst[i] < pivot): i += 1
+            while(lst[j] > pivot): j -= 1
+            if i <= j:
+                lst[i],lst[j] = lst[j],lst[i]
+                i += 1
+                j -= 1
             else:
                 break
-        lst[right] , lst[j]  = lst[j] , lst[right]
-        Quick_sort2(lst,left,j-1)
-        Quick_sort2(lst,j+1,right)
+            print(lst)
+        lst[right] , lst[i]  = lst[i] , lst[right]
+        Quick_sort2(lst,left,i-1)
+        Quick_sort2(lst,i+1,right)
 
-
-lst = [3,4,7,1,5,7, 99, 100 , 8]
+def Quick_sort3(lst , left , right):
+    if left >= right:
+        return 
+    else:
+        pivot = lst[int((left + right)/2)]
+        print(pivot)
+        i = left
+        j = right
+        while(j>=i):
+            while(lst[i]< pivot): i += 1
+            while(lst[j]> pivot): j -= 1
+            if (j>=i):
+                lst[i] , lst[j] = lst[j] , lst[i]
+                i += 1
+                j -= 1
+            else:
+                break 
+            print(lst)
+        if (left < j):
+            Quick_sort3(lst, left, j)
+        if (i < right):
+            Quick_sort3(lst, i, right)       
+lst = [1, 12, 5, 26, 7, 2, 3, 7, 2]
 print(lst)
-Quick_sort1(lst , 0 , 8)
+Quick_sort3(lst , 0 , 8)
 print(lst)
 
 
