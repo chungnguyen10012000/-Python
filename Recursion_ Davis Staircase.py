855f946388bf874bb408a9f02c7ce3811b7a8df4
Nguyễn Văn Chung <56922083+chungnguyen10012000@users.noreply.github.com>
2021-03-16 10:09:05 +0700

    recursion fibonaci sequence with performence O(n)

---
+++ Recursion_ Davis Staircase.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the stepPerms function below.
+def stepPerms(n):
+    f1, f2, f3 = 1, 2, 4
+    for i in range(n-1):
+        f1, f2, f3 = f2, f3, f1 + f2 + f3
+    return f1
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = int(input())
+
+    for s_itr in range(s):
+        n = int(input())
+
+        res = stepPerms(n)
+
+        fptr.write(str(res) + '\n')
+
+    fptr.close()
